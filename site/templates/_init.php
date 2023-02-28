2d52f88ba0f1e9a17b92f829f625e6d90c2798e9
Notitguy Office <kelvin77@seznam.cz>
2023-02-28 15:56:14 +0100

    Add Parcel, Concurrently

--- site/templates/_init.php
+++ site/templates/_init.php
@@ -4,5 +4,6 @@
 // This is defined by $config->prependTemplateFile in /site/config.php.
 // Use this to define shared variables, functions, classes, includes, etc. 
 
-$assets = $config->urls->templates . "assets/";
+$assets = $config->urls->templates . "assets/"; // not used? see _head
+$dist = $config->urls->root . "dist/";
 $components = $config->paths->templates . "inc/components/";
