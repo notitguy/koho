66a3ee3c6f00d5beb3f7062835e7febdd5571ce7
Notitguy Office <kelvin77@seznam.cz>
2023-03-14 17:53:48 +0100

    plate making

---
+++ site/templates/inc/components/plate.php
@@ -0,0 +1,45 @@
+<?php namespace ProcessWire;
+
+  $plateImage = $assets ."images/main/plate.png";
+
+  $p1 = $assets ."images/main/peanuts/p1.png";
+  $p2 = $assets ."images/main/peanuts/p2.png";
+  $p3 = $assets ."images/main/peanuts/p3.png";
+  $p4 = $assets ."images/main/peanuts/p4.png";
+  $p5 = $assets ."images/main/peanuts/p5.png";
+  $p6 = $assets ."images/main/peanuts/p6.png";
+  $p7 = $assets ."images/main/peanuts/p7.png";
+  $p8 = $assets ."images/main/peanuts/p8.png";
+  $p9 = $assets ."images/main/peanuts/p9.png";
+  $p10 = $assets ."images/main/peanuts/p10.png";
+  $p11 = $assets ."images/main/peanuts/p11.png";
+  $p12 = $assets ."images/main/peanuts/p12.png";
+  $p13 = $assets ."images/main/peanuts/p13.png";
+  $p14 = $assets ."images/main/peanuts/p14.png";
+  $p15 = $assets ."images/main/peanuts/p15.png";
+  $p16 = $assets ."images/main/peanuts/p16.png";
+  $p17 = $assets ."images/main/peanuts/p17.png";
+
+?>
+
+<div class="hero-cockerel">
+  <img src="<?= $p1 ?>" width="56" alt="">
+  <img src="<?= $p2 ?>" width="56" alt="">
+  <img src="<?= $p3 ?>" width="56" alt="">
+  <img src="<?= $p4 ?>" width="56" alt="">
+  <img src="<?= $p5 ?>" width="56" alt="">
+  <img src="<?= $p6 ?>" width="56" alt="">
+  <img src="<?= $p7 ?>" width="56" alt="">
+  <img src="<?= $p8 ?>" width="56" alt="">
+  <img src="<?= $p9 ?>" width="56" alt="">
+  <img src="<?= $p10 ?>" width="56" alt="">
+  <img src="<?= $p11 ?>" width="56" alt="">
+  <img src="<?= $p12 ?>" width="56" alt="">
+  <img src="<?= $p13 ?>" width="56" alt="">
+  <img src="<?= $p14 ?>" width="56" alt="">
+  <img src="<?= $p15 ?>" width="56" alt="">
+  <img src="<?= $p16 ?>" width="56" alt="">
+  <img src="<?= $p17 ?>" width="56" alt="">
+
+  <img src="<?= $plateImage ?>" width="800" class="plate" alt="">
+</div>
