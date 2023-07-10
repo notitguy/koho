c5fc5ccf67e7ff47a921448e893ddc91384fc789
notitguy <notitguy@email.com>
2023-07-10 13:09:08 +0200

    Add multiple video block

--- site/templates/inc/sections/about.php
+++ site/templates/inc/sections/about.php
@@ -12,10 +12,24 @@ $galleryIcon = $assets ."/icons/gallery.svg";
   <?php include ($components ."running-text.php") ?>
   <?php include ($components ."eventlist.php") ?>
   <article id="about" class="container">
-    <div class="about__video" style="padding:50vmin 50% 0 50%;position:relative;">
+    <!-- <div class="about__video" style="padding:50vmin 50% 0 50%;position:relative;">
       <iframe src="https://player.vimeo.com/video/772986451?h=5ed232e1ec&autoplay=1&loop=1&byline=0&portrait=0&muted=1&controls=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture"></iframe>
       <script src="https://player.vimeo.com/api/player.js" async defer></script>
-    </div>
+    </div> -->
+    <div class="about__video2">
+      <div class="body">
+        <h3><?= $videoBlockTitle ?></h3>
+        <p><?= $videoBlockBody ?></p>
+      </div>
+      <div class="videos">
+        <video autoplay muted loop >
+          <source src="<?= $assets . "video/IMG_0731.MP4" ?>" type=video/mp4>
+        </video>
+        <video autoplay muted loop >
+          <source src="<?= $assets . "video/IMG_0732.MP4" ?>" type=video/mp4>
+        </video>
+      </div>
+      </div>
     <div class="about__body">
       <h2><?= $page->heading_about ?></h2>
       <?= $page->body_about ?>
