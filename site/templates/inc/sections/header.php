1268a62e81161065380c15abd2f3905178fd7643
notitguy <notitguy@email.com>
2023-08-01 00:59:29 +0200

    gay logo on

--- site/templates/inc/sections/header.php
+++ site/templates/inc/sections/header.php
@@ -18,7 +18,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
       <a href="#footer"><?= $findUs ?></a>
     </nav>
     <a href="<?= $home->url?>" class="logo">
-      <img src="<?= $logo ?>" width="115" alt="Kohoutek">
+      <img src="<?= $logoGay ?>" width="115" alt="Kohoutek">
     </a>
     <nav class="mobile">
       <a href="<?= $home->url?>" class="mobile-logo">
