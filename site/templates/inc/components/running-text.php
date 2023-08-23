5d4e092da885160455d9b590ffaa046091d9e633
notitguy <notitguy@email.com>
2023-08-23 10:56:33 +0200

    git ignore cnfg php

--- site/templates/inc/components/running-text.php
+++
@@ -1,5 +0,0 @@
-<div class="track">
-  <div class="running-text">
-    <h2><?= $runningText; ?></h2>
-  </div>
-</div>
--- site/templates/redirect.php
+++
@@ -1,3 +0,0 @@
-<?php namespace ProcessWire;
-
-$session->redirect('https://speedlo.cz/app/kohoutek/wb/home');
