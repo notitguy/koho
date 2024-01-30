1fdfade86a22affb2afe9ed3d8f1d37065b8cb19
Notitguy Office <kelvin77@seznam.cz>
2024-01-30 16:18:49 +0100

    Change GTM

--- site/templates/inc/sections/_head.php
+++ site/templates/inc/sections/_head.php
@@ -9,13 +9,13 @@
   ?>
   <link rel="stylesheet" type="text/css" href="<?php echo $dist; ?>_main.css" />
 
-  <!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-W8CZBP2');</script>
-  <!-- End Google Tag Manager -->
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TR9C8MLN');</script>
+<!-- End Google Tag Manager -->
   
   <link rel="apple-touch-icon" sizes="180x180" href="<?= $assets. "favicon/apple-touch-icon.png"; ?>" >
   <link rel="icon" type="image/png" sizes="32x32" href="<?= $assets. "favicon/favicon-32x32.png"; ?>" >
