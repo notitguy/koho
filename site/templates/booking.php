b64b4141cd6409d9829e122663f7e3581ea48df0
notitguy <notitguy@email.com>
2024-07-07 02:48:02 +0200

    add table booking template (not used)

---
+++ site/templates/booking.php
@@ -0,0 +1,14 @@
+<?php namespace ProcessWire;
+
+?>
+
+<main pw-id="content">
+  <div class="border --80"><div></div></div>
+  <section>
+    <article class="container" style="width: 100%">
+      <div style="display: flex">
+        <iframe src="https://embed.choiceqr.com/booking/kohoutek?lang=cz" style="border:none;width:360px;max-width:100%;min-height:620px;margin-inline:auto"></iframe>
+      </div>
+    </article>
+  </section>
+</main>
